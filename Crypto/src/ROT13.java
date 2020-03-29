ac1a049f7c226aa1c4489c213a27baef1155ba16
chipfody <60615532+chipfody@users.noreply.github.com>
2020-03-29 17:27:21 -0400

    decrypted

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,30 +3,47 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    private Integer key;
 
     ROT13(Character cs, Character cf) {
+        this.key = cf - cs;
     }
 
     ROT13() {
+        this.key = 0;
     }
 
-
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        StringBuilder crypto = new StringBuilder (  );
+        for (int i = 0; i < text.length(); i++) {
+            char s = text.charAt(i);
+            if (s >='a' && s <= 'm') {
+                s += 13;
+            } else if (s >= 'n' && s <= 'z') {
+                s -= 13;
+            } else if (s >= 'A' && s <= 'M') {
+                s += 13;
+            } else if ( s >= 'N' && s <= 'Z') {
+                s -= 13;
+            }
+            crypto.append ( s );
+        }
+        return crypto.toString ();
     }
 
     public String encrypt(String text) {
-        return text;
+        String result = crypt ( text );
+        return result;
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
+        Integer rotateOn = s.indexOf ( c );
 
-        return "";
+        return s.substring ( rotateOn ) + s.substring ( 0, rotateOn );
     }
 
 }
